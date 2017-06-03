f57df532b0bbb47cd8d2a1652248448db1d228e7
madhuroopa <madhuroopa246@gmail.com>
2017-06-03 10:48:15 +0530

    added

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,42 +4,94 @@ import game from './game';
 @Component({
     selector: 'my-app',
     template: `
-    <h1>Tic Tac Toe</h1>
+   <center> <h1>RUNMAN</h1></center>
     <div class="board">
         <div *ngFor="#row of board;#rowNo=index" class="row">
-            <div *ngFor="#cell of row;#colNo=index" class="cell" (click)="cellClicked(rowNo, colNo)">
-                <span class="marker" *ngIf="cell===0">X</span>
-                <span class="marker" *ngIf="cell===1">O</span>
-            </div>
-        </div>
-    </div>
-    <div class="gameResults" *ngIf="winner===0">Winner: First Player</div>
-    <div class="gameResults" *ngIf="winner===1">Winner: Second Player</div>
-    <button class="restart" (click)="restart()">Restart</button>`
+            <div *ngFor="#cell of row;#colNo=index" class="cell" >
+             <div class = "flag" *ngIf = "rowNo == 0 && colNo == 0">
+               <img src = "flag.png">
+             </div>
+             <div *ngIf = "rowNo == 0 && colNo == 2">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 3 && colNo == 4">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 1 && colNo == 5">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 5 && colNo == 2">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 2 && colNo == 5">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 6 && colNo == 2">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 2 && colNo == 4">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 4 && colNo == 2">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 6 && colNo == 7">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 5 && colNo == 7">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 4 && colNo == 7">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 7 && colNo == 7">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 5 && colNo == 1">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 6 && colNo == 1">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 7 && colNo == 1">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 0 && colNo == 8">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 0 && colNo == 9">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 5 && colNo == 9">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 6 && colNo == 9">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 7 && colNo == 5">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 10 && colNo == 5">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 9 && colNo == 0">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 2 && colNo == 9">
+               <img src = "tree.png">
+             </div>
+             <div *ngIf = "rowNo == 11 && colNo == 0">
+              <div class = "image">
+                <img src = "hero.png">
+               </div>
+             </div>
+         </div>
+     </div> 
+ </div>
+    `
 })
 
-export class AppComponent {
-    public currentPlayer = 0;
-    public board = game.createBoard();
-    public winner;
-
-    restart() {
-        this.board = game.createBoard();
-        this.winner = null;
-        this.currentPlayer = 0;
-    }
-
-    cellClicked(row, col) {
-        if (this.board[row][col] === 0 || this.board[row][col] === 1 || this.winner != null)
-            return;
-
-        this.currentPlayer++;
-        this.board[row][col] = this.currentPlayer % 2 ? 0 : 1;
-
-        var checkResult = game.checkBoard(this.board);
-
-        if (checkResult) {
-            this.winner = checkResult.winner;
-        }
-    }
+export class AppComponent { 
+    public board = game.createBoard();   
+    
 }
--- app/cell.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: '.cell',
-    template: '<span class="marker">X</span>'
-})
-export class Cell {
-}
