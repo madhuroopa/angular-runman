1c25bc98f29766e22f387df4b0eaf424fe7c9e4a
PrasannaRao <raoprasanna09@gmail.com>
2017-06-07 12:23:19 +0530

    added start and stop to the timer

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,22 @@
 import {Component,OnInit} from 'angular2/core';
 import game from './game';
+import { TimerService } from './timer.service';
 import {Subscription} from 'rxjs/Rx';
 import { Observable} from 'rxjs/Rx';
 
 @Component({
+    providers: [ TimerService ],
     selector: 'my-app',
     template: `
     <center><h1>Run Man</h1></center>
     <center> <h2>
           {{(minutesDisplay) && (minutesDisplay <= 59) ? minutesDisplay : '00'}} : {{(secondsDisplay) && (secondsDisplay <= 59) ? secondsDisplay : '00'}} <br/>
         </h2></center>
- 
-    <div class="board">
+     <div>
+          <button [disabled]="start" (click)="startTimer()"type="button" class="btn btn-success btn-circle btn-xl"><i class="glyphicon glyphicon-link"></i>start</button>
+          <button [disabled]="stop" (click)="stopTimer()"type="button" class="btn btn-danger btn-circle btn-xl1"><i class="glyphicon glyphicon-link"></i>stop</button>
+   </div>
+   <div class="board">
         <div *ngFor="#row of board;#rowNo=index" class="row">
             <div *ngFor="#cell of row;#colNo=index" class="cell" (click)="cellClicked(rowNo, colNo)">
              <div *ngIf = "rowNo == 0 && colNo == 0">
@@ -104,24 +109,34 @@ import { Observable} from 'rxjs/Rx';
     `
    
 })
-export class AppComponent implements OnInit{ 
-  public board = game.createBoard();   
-  ticks = 0;
-  minutesDisplay: number = 0;
-  secondsDisplay: number = 0;
-  sub:Subscription<any>;
-  ngOnInit() {
-    this.startTimer();
-  }
-  private startTimer() {
-    let timer = Observable.timer(1, 1000);
-    this.sub = timer.subscribe(
+export class AppComponent  implements OnInit, OnDestroy{ 
+   public board = game.createBoard();   
+   ticks = 0;
+   start = 0;
+   private playPauseStopUnsubscribe: any;
+    private play: boolean;
+
+    constructor(private timerService: TimerService) {
+    }
+ 
+   minutesDisplay: number = 0;
+   secondsDisplay: number = 0;
+   sub:Subscription<any>;
+   OnInit() {
+     this.startTimer();
+   }
+   private startTimer() {
+     let timer = Observable.timer(1, 1000);
+     this.sub = timer.subscribe(
       t => {
-        this.ticks = t;
-        this.secondsDisplay = this.getSeconds(this.ticks);
-        this.minutesDisplay = this.getMinutes(this.ticks);
-      });
+       this.ticks = t;
+       this.secondsDisplay = this.getSeconds(this.ticks);
+       this.minutesDisplay = this.getMinutes(this.ticks);
+       
+      }
+    );
   }
+  
   private getSeconds(ticks: number) {
     let seconds  = 120;
     if (this.ticks > seconds) {
@@ -135,4 +150,31 @@ export class AppComponent implements OnInit{
   private pad(digit: any) { 
     return digit <= 9 ? '0' + digit : digit;
   }
-} 
+ 
+    ngOnInit() {
+        this.playPauseStopUnsubscribe = this.timerService.playPauseStop$.subscribe((res: any) => this.setPlay(res));       
+    }
+
+    ngOnDestroy() {
+        this.playPauseStopUnsubscribe.unsubscribe();
+    }
+
+    private setPlay(res: any) {
+        (res.play) ? this.play = true : this.play = false;
+    }
+
+    playTimer() {
+        this.timerService.playTimer();
+    }
+
+     stopTimer() {
+        this.timerService.stopTimer();
+         this.start = 0;
+        this.ticks = 0;
+        this.minutesDisplay = 0;
+        
+        this.secondsDisplay = 0;
+        if (this.sub) this.sub.unsubscribe();
+    }
+
+}
